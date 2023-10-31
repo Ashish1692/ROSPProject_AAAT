184208dc77d754be5ad5d1d9d702c0c01baa1147
Ashish1692 <ashishmoghe6@gmail.com>
2023-10-31 20:30:40 +0530

    v2 - pwa

---
+++ serviceWorker.js
@@ -0,0 +1,28 @@
+const staticDevCoffee = "eecalc-site-v1"
+const assets = [
+    "/",
+    "/index.html",
+    "/index.css",
+    "/index.js",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(staticDevCoffee).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("activate", evt => {
+    console.log("activated");
+});
+
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
